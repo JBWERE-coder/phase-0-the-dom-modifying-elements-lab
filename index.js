6bc2ac8914fd4683af628b7fc8075317eaa260fd
JBWERE-coder <lutherwere@gmail.com>
2023-06-19 14:46:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.getElementById("main")
+element.remove()
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Your-Name is the champion"
